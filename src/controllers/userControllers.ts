a03b0e244cac3128cd6b056d8642d0aec91c059e
rishisoyal <rishisoyal510@gmail.com>
2025-11-28 13:42:32 +0530

    set node env

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -2,59 +2,60 @@ import bcrypt from "bcrypt";
 import { SignJWT } from "jose";
 import Express from "express";
 import User from "../models/UserModel";
-import dotenv from "dotenv";
+import {config} from "dotenv";
 
-dotenv.config(); // load once
+config(); // load once
 
 export async function userLogIn(req: Express.Request, res: Express.Response) {
-  const { name, password } = req.body;
+	const { name, password } = req.body;
 
-  if (!name || !password) {
-    return res.status(400).json({ error: "Missing credentials" });
-  }
+	if (!name || !password) {
+		return res.status(400).json({ error: "Missing credentials" });
+	}
 
-  const user = await User.findOne({ name });
-  if (!user) return res.status(401).json({ error: "User not found" });
+	const user = await User.findOne({ name });
+	if (!user) return res.status(401).json({ error: "User not found" });
 
-  const valid = await bcrypt.compare(password, user.password_hash);
-  if (!valid) return res.status(401).json({ error: "Invalid credentials" });
+	const valid = await bcrypt.compare(password, user.password_hash);
+	if (!valid) return res.status(401).json({ error: "Invalid credentials" });
 
-  const token = await new SignJWT({ uid: user._id.toString(), role: user.role })
-    .setProtectedHeader({ alg: "HS256" })
-    .setExpirationTime("2d")
-    .sign(new TextEncoder().encode(process.env.TOKEN_SECRET));
+	const token = await new SignJWT({ uid: user._id.toString(), role: user.role })
+		.setProtectedHeader({ alg: "HS256" })
+		.setExpirationTime("2d")
+		.sign(new TextEncoder().encode(process.env.TOKEN_SECRET));
 
-  const isProd = process.env.NODE_ENV === "production";
-
-  return res
-    .cookie("auth_token", token, {
-      httpOnly: true,
-      secure: isProd,
-      sameSite: "none",
-      path: "/",         // MUST MATCH logout
-      maxAge: 2 * 24 * 60 * 60 * 1000, // 2 days
-    })
-    .status(200)
-    .json({ message: "success" });
+	const isProd = process.env.NODE_ENV === "production";
+	// console.log(process.env.NODE_ENV);
+	
+	console.log("user login")
+	return res
+		.cookie("auth_token", token, {
+			httpOnly: true,
+			secure: isProd,
+			sameSite: "none",
+			path: "/", // MUST MATCH logout
+			maxAge: 2 * 24 * 60 * 60 * 1000, // 2 days
+		})
+		.status(200)
+		.json({ message: "success" });
 }
 
 export async function userLogOut(req: Express.Request, res: Express.Response) {
-  const token = req.cookies.auth_token;
-	console.log(token);
+	const token = req.cookies.auth_token;
+	//console.log(token);
 
+	if (!token) return res.status(401).json({ error: "Token not found" });
 
-  if (!token) return res.status(401).json({ error: "Token not found" });
-
-  const isProd = process.env.NODE_ENV === "production";
-
-  return res
-    .cookie("auth_token", "", {
-      httpOnly: true,
-      secure: isProd,
-      sameSite: false,
-      path: "/",
-      expires: new Date(0),
-    })
-    .status(200)
-    .json({ message: "logged out" });
+	const isProd = process.env.NODE_ENV === "production";
+	console.log("user logout")
+	return res
+		.cookie("auth_token", "", {
+			httpOnly: true,
+			secure: isProd,
+			sameSite: "none",
+			path: "/",
+			expires: new Date(0),
+		})
+		.status(200)
+		.json({ message: "logged out" });
 }
--- src/index.ts
+++ src/index.ts
@@ -11,48 +11,49 @@ config();
 const MONGODB_URI = process.env.MONGODB_URI;
 const port = process.env.PORT;
 if (!MONGODB_URI) {
-  console.error("'MONGODB_URI' not found in the .env file");
-  process.exit(1);
+	console.error("'MONGODB_URI' not found in the .env file");
+	process.exit(1);
 }
 
 mongoose
-  .connect(MONGODB_URI, {
-    dbName: "company_website",
-  })
-  .then(() => {
-    console.log("Successfully connected to the database");
-  })
-  .catch((err: mongoose.Error) => {
-    console.error(err.message);
-  });
+	.connect(MONGODB_URI, {
+		dbName: "company_website",
+	})
+	.then(() => {
+		console.log("Successfully connected to the database");
+	})
+	.catch((err: mongoose.Error) => {
+		console.error(err.message);
+	});
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow non-browser tools like Postman (no origin)
-      if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS: " + origin));
-      }
-    },
-    credentials: true,
-  })
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
+app.set("trust proxy", 1); // important for secure cookies behind proxy
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			if (!origin) return callback(null, true);
+			if (allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS: " + origin));
+			}
+		},
+		credentials: true,
+	}),
 );
+
 app.use(cookieParser());
 app.use(fileUpload());
 app.use("/api/content", ContentRouter);
 app.use("/api/user", UserRouter);
 
 app.listen(port, (err) => {
-  if (err) {
-    console.error(err);
-  }
-  console.log(`App running at http://localhost:${port}`);
+	if (err) {
+		console.error(err);
+	}
+	console.log(`App running at http://localhost:${port}`);
 });
